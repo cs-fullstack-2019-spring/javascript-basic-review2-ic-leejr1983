9b20c22abf215fcb8c4a90ee36e4e590fe7b0e01
leejr1983 <leejr1983@yahoo.com>
2019-01-14 10:28:30 -0600

    classwork

---
+++ Problem1.js
@@ -0,0 +1,13 @@
+// Create two variables called five assigned 5 and eight assigned 8.
+// Next create an if statement compairing five is less than eight.
+// If it's true, , print “5 is less than 8”. Else, print “ERROR”.
+
+five= parseInt(5)
+eight= parseInt(8)
+
+if (five < eight){
+    console.log ("5 is less than 8")
+}
+else {
+    console.log ("ERROR")
+}
